fd26d4a4f81f3ee2eceddaa394f379112046aca3
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-10-18 14:28:19 +0300

    Extract components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
+
 function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <main id="main-content">
+                <Home />
+            </main>
+        </div>
+    );
 }
 
 export default App;
