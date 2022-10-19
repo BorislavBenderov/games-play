6bbb966410bd3c380de89343895bcca85140a488
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-10-19 22:14:23 +0300

    Make controlled forms on Edit component

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,26 +1,49 @@
+import { useState, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { GameContext } from "../../contexts/GameContext";
+
 export const Edit = () => {
+    const { games } = useContext(GameContext);
+    const { gameId } = useParams();
+    const game = games.find(x => x._id === gameId);
+
+    const [values, setValues] = useState({
+        title: game.title,
+        category: game.category,
+        maxLevel: game.maxLevel,
+        imageUrl: game.imageUrl,
+        summary: game.summary
+    })
+
+    const changeHandler = (e) => {
+        setValues(oldState => ({
+            ...oldState,
+            [e.target.name]: e.target.value
+        }));
+    }
+
     return (
         <section id="edit-page" className="auth">
             <form id="edit">
                 <div className="container">
                     <h1>Edit Game</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
-                    <input type="text" id="title" name="title" defaultValue="" />
+                    <input type="text" id="title" name="title" value={values.title} onChange={changeHandler} />
                     <label htmlFor="category">Category:</label>
-                    <input type="text" id="category" name="category" defaultValue="" />
+                    <input type="text" id="category" name="category" value={values.category} onChange={changeHandler}/>
                     <label htmlFor="levels">MaxLevel:</label>
                     <input
                         type="number"
                         id="maxLevel"
                         name="maxLevel"
                         min={1}
-                        defaultValue=""
+                        value={values.maxLevel} onChange={changeHandler}
                     />
                     <label htmlFor="game-img">Image:</label>
-                    <input type="text" id="imageUrl" name="imageUrl" defaultValue="" />
+                    <input type="text" id="imageUrl" name="imageUrl" value={values.imageUrl} onChange={changeHandler}/>
                     <label htmlFor="summary">Summary:</label>
-                    <textarea name="summary" id="summary" defaultValue={""} />
-                    <input className="btn submit" type="submit" defaultValue="Edit Game" />
+                    <textarea name="summary" id="summary" value={values.summary} onChange={changeHandler}/>
+                    <input className="btn submit" type="submit" value="Edit Game" />
                 </div>
             </form>
         </section>
